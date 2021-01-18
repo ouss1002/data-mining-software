d7907452fa33cbb5fc2bd4620fd5ec449710d3d7
billel mokeddem <bilel.crysis3@gmail.com>
2021-01-18 21:48:33 +0100

    Creating the Instance class

---
+++ src/main/java/datamining/Instance.java
@@ -0,0 +1,12 @@
+package datamining;
+
+import java.util.ArrayList;
+
+public class Instance {
+    ArrayList<Variable> variables;
+
+    public Instance(ArrayList<Variable> variables) {
+        this.variables = variables;
+    }
+
+}
