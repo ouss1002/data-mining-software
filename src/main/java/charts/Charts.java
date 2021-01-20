8f14bbd65bff8d25d3930089946802b8d368e9e8
billel mokeddem <bilel.crysis3@gmail.com>
2021-01-20 19:25:08 +0100

    Adding saving as png fanctionality

--- src/main/java/charts/Charts.java
+++ src/main/java/charts/Charts.java
@@ -4,6 +4,7 @@ import datamining.DataSet;
 import org.knowm.xchart.*;
 import org.knowm.xchart.style.BoxStyler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,16 +14,17 @@ public final class Charts {
 
     }
 
-    public static BoxChart BoxPlot(DataSet dataset, String name) {
+    public static BoxChart BoxPlot(DataSet dataset, String name) throws IOException {
         ArrayList<Double> column = dataset.getColumn(name);
         BoxChart chart = new BoxChartBuilder().title("Box Plot").build();
         chart.getStyler().setBoxplotCalCulationMethod(BoxStyler.BoxplotCalCulationMethod.N_LESS_1_PLUS_1);
         chart.getStyler().setToolTipsEnabled(true);
         chart.addSeries(name, column);
+        BitmapEncoder.saveBitmapWithDPI(chart, "./BoxPlot_300_DPI", BitmapEncoder.BitmapFormat.PNG, 300);
         return chart;
     }
 
-    public static CategoryChart Histogram(DataSet dataset, String name) {
+    public static CategoryChart Histogram(DataSet dataset, String name) throws IOException {
         ArrayList<Double> column = dataset.getColumn(name);
         HashMap<Double, Integer> counting = new HashMap<>();
 
@@ -36,10 +38,11 @@ public final class Charts {
         chart.getStyler().setToolTipsEnabled(false);
         chart.getStyler().setLegendVisible(false);
         chart.addSeries(name, new ArrayList<>(counting.keySet()), new ArrayList<>(counting.values()));
+        BitmapEncoder.saveBitmapWithDPI(chart, "./Histogram_300_DPI", BitmapEncoder.BitmapFormat.PNG, 300);
         return chart;
     }
 
-    public static XYChart ScatterPlot(DataSet dataset, String name1, String name2) {
+    public static XYChart ScatterPlot(DataSet dataset, String name1, String name2) throws IOException {
         ArrayList<Double> column1 = dataset.getColumn(name1);
         ArrayList<Double> column2 = dataset.getColumn(name2);
         XYChart chart = new XYChartBuilder().width(600).height(500).title("Scatter Plot").xAxisTitle("X").yAxisTitle("Y").build();
@@ -49,7 +52,7 @@ public final class Charts {
         chart.getStyler().setMarkerSize(13);
 
         chart.addSeries(name1+ " " + name2, column1, column2);
-
+        BitmapEncoder.saveBitmapWithDPI(chart, "./ScatterPlot_300_DPI", BitmapEncoder.BitmapFormat.PNG, 300);
         return chart;
     }
 }
