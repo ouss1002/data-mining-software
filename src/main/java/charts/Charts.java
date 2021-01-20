d1c32467a9b335610f8b94d7dee574b32cfb4d87
billel mokeddem <bilel.crysis3@gmail.com>
2021-01-20 18:36:36 +0100

    Creating a Histogram method in Charts

--- src/main/java/charts/Charts.java
+++ src/main/java/charts/Charts.java
@@ -3,6 +3,8 @@ package charts;
 import datamining.DataSet;
 import org.knowm.xchart.BoxChart;
 import org.knowm.xchart.BoxChartBuilder;
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.CategoryChartBuilder;
 import org.knowm.xchart.style.BoxStyler;
 
 import java.util.ArrayList;
@@ -22,5 +24,20 @@ public final class Charts {
         chart.addSeries(name, column);
         return chart;
     }
+    
+    public static CategoryChart Histogram(DataSet dataset, String name) {
+        ArrayList<Double> column = dataset.getColumn(name);
+        HashMap<Double, Integer> counting = new HashMap<>();
 
+        for (Double c: column) {
+            if (!counting.containsKey(c)) {
+                counting.put(c, 0);
+            }
+            counting.put(c, counting.get(c) + 1);
+        }
+        CategoryChart chart = new CategoryChartBuilder().title("Histogram").build();
+        chart.getStyler().setToolTipsEnabled(false);
+        chart.addSeries(name, new ArrayList<>(counting.keySet()), new ArrayList<>(counting.values()));
+        return chart;
+    }
 }
