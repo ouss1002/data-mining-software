52968dbef6ca5f8f2c96c9f3e85defff69cc9890
ouss1002 <oboudjeniba@gmail.com>
2021-01-20 15:08:08 +0100

    Add getAttributes()

--- src/main/java/gui/app/ApplicationController.java
+++ src/main/java/gui/app/ApplicationController.java
@@ -2,20 +2,18 @@ package gui.app;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTreeTableView;
 import datamining.DataSet;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 
 
-import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ApplicationController {
 
@@ -26,7 +24,7 @@ public class ApplicationController {
     private TableView<?> tableBoxTab;
 
     @FXML
-    private JFXComboBox<?> cbDatasetTabAttribute;
+    private JFXComboBox cbDatasetTabAttribute;
 
     @FXML
     private Label lblDatasetType;
@@ -68,9 +66,6 @@ public class ApplicationController {
     private Label lblBoxTabMax;
 
     @FXML
-    private JFXTreeTableView<?> tableBoxTab;
-
-    @FXML
     private JFXComboBox<?> cbHistogramTabAttribute;
 
     @FXML
@@ -92,27 +87,35 @@ public class ApplicationController {
     private AnchorPane apHistoTab;
 
     public FileChooser fileChooserObject = new FileChooser();
-    DataSet dataset;
+    public DataSet dataset;
 
     public ApplicationController() {
 
     }
 
-    public void initialize() {
+    public void initialize() throws IOException {
         fileChooserObject.getExtensionFilters().add(new FileChooser.ExtensionFilter("files", "*.txt"));
 
-        btnDatasetImport.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent actionEvent2) {
-                File selectedFile = fileChooserObject.showOpenDialog(null);
+        prepare();
+    }
 
-                try {
-                     dataset = new DataSet(selectedFile.getAbsolutePath());
-                     //System.out.println("perfect");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    public void prepare() throws IOException {
+
+        tableDataset.getItems().clear();
+        dataset = new DataSet("ADD FILE PATH HERE");
+        cbDatasetTabAttribute.setItems(this.getAttributes());
+
+    }
+
+    public ObservableList<String>  getAttributes() {
+        ArrayList<String> names = dataset.getColumnNames();
+        ObservableList<String> choices = FXCollections.observableArrayList(names);
+
+        return choices;
+    }
+
+    public void fillDatasetTable() {
+        // TODO: fill the table after importing the dataset
     }
 
 
