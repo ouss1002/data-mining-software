68e19a227027da2638ad53a0bd76da2fd35c10b6
billel mokeddem <bilel.crysis3@gmail.com>
2021-03-17 16:57:07 +0100

    Create the Apriori class

---
+++ src/main/java/algorithms/Apriori/Apriori.java
@@ -0,0 +1,21 @@
+package algorithms.Apriori;
+
+import datamining.Instance;
+import datamining.Variable;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashMap;
+
+public class Apriori {
+    public static LinkedHashMap<Integer, ArrayList<String>> transactionDB = new LinkedHashMap<>();
+    static ArrayList<Instance> classInstances = new ArrayList<>();
+    
+    public static void main(String[] args){
+        //generateTransactionDB();
+    }
+}
+
+
