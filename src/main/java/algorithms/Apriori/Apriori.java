c8d52527ef8cd78b002fbdde62b453ac57516f11
billel mokeddem <bilel.crysis3@gmail.com>
2021-03-17 16:58:18 +0100

    Create the generateTransactionDB method
    to generate transaction list from an array of instances

--- src/main/java/algorithms/Apriori/Apriori.java
+++ src/main/java/algorithms/Apriori/Apriori.java
@@ -12,7 +12,19 @@ import java.util.LinkedHashMap;
 public class Apriori {
     public static LinkedHashMap<Integer, ArrayList<String>> transactionDB = new LinkedHashMap<>();
     static ArrayList<Instance> classInstances = new ArrayList<>();
-    
+
+    public static void generateTransactionDB(ArrayList<Instance> instances) {
+        classInstances = instances;
+        for (Instance instance : classInstances) {
+            ArrayList<String> itemSet = new ArrayList<>();
+            ArrayList<Variable> variables = instance.getVariables();
+            for (int i = 0; i < variables.size(); i++) {
+                itemSet.add(i + "_" + variables.get(i).get());
+            }
+            transactionDB.put(instance.getInstanceNumber(), itemSet);
+        }
+    }
+
     public static void main(String[] args){
         //generateTransactionDB();
     }
